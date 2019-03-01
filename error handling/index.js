7bd4de763f25f0d22df4294018d1ce3709d22bb4
hollywardlaw <holly_wardlaw@hotmail.co.uk>
2019-03-01 12:57:46 +0000

    removed all commented out code and cnsole logs and added a console log to err 500

--- error handling/index.js
+++ error handling/index.js
@@ -21,5 +21,6 @@ exports.handle422 = (err, req, res, next) => {
 };
 
 exports.handle500 = (err, req, res, next) => {
+  console.log(err);
   res.status(500).send({ msg: 'Something broke :(' });
 };
