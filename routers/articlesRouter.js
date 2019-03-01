958b233fc30f30234606c5a99930da441a93fa29
hollywardlaw <holly_wardlaw@hotmail.co.uk>
2019-03-01 12:50:08 +0000

    Used .route to tidy up routers

--- routers/articlesRouter.js
+++ routers/articlesRouter.js
@@ -5,14 +5,18 @@ const {
 const { sendCommentsByArticleID } = require('../controllers/commentCon');
 const { handle405 } = require('../error handling/index');
 
+articlesRouter.route('/')
+  .get(sendArticles)
+  .post(postArticles)
+  .all(handle405);
 
-articlesRouter.get('/', sendArticles);
-articlesRouter.get('/:article_id', sendArticles);
-articlesRouter.post('/', postArticles);
-articlesRouter.patch('/:article_id', patchArticle);
-articlesRouter.delete('/:article_id', deleteArticle);
-articlesRouter.get('/:article_id/comments', sendCommentsByArticleID);
-articlesRouter.all('/', handle405);
+articlesRouter.route('/:article_id')
+  .get(sendArticles)
+  .patch(patchArticle)
+  .delete(deleteArticle);
+
+articlesRouter.route('/:article_id/comments')
+  .get(sendCommentsByArticleID);
 
 
 module.exports = articlesRouter;
--- routers/topicsRouter.js
+++ routers/topicsRouter.js
@@ -2,8 +2,9 @@ const topicsRouter = require('express').Router();
 const { sendTopics, postTopic } = require('../controllers/topicCon');
 const { handle405 } = require('../error handling/index');
 
-topicsRouter.get('/', sendTopics);
-topicsRouter.post('/', postTopic);
-topicsRouter.all('/', handle405);
+topicsRouter.route('/')
+  .get(sendTopics)
+  .post(postTopic)
+  .all(handle405);
 
 module.exports = topicsRouter;
