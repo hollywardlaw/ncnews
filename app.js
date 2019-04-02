27736edb47d0b808187186e6dc8578ad2bb87914
Holly Wardlaw <hollywardlaw@Matts-MacBook-Pro.local>
2019-04-02 16:48:58 +0100

    enabled CORS

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const app = require('express')();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const apiRouter = require('./routers/apiRouter');
 const {
-  handle404, handle400, handle422, handle500,
+  handle404,
+  handle400,
+  handle422,
+  handle500
 } = require('./error-handling/index');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
